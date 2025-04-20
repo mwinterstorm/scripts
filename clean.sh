a91d0d85b71fb043a76837551c7c7f4db4eb9912
mwinterstorm <mark.wr@gmail.com>
2025-04-21 10:22:40 +1200

    add ability to keep old kernals

--- clean.sh
+++ clean.sh
@@ -51,7 +51,9 @@ OLDCONF=$(dpkg -l|grep "^rc"|awk '{print $2}')
 CURKERNEL=$(uname -r|sed 's/-*[a-z]//g'|sed 's/-386//g')
 LINUXPKG="linux-(image|headers|ubuntu-modules|restricted-modules)"
 METALINUXPKG="linux-(image|headers|restricted-modules)-(generic|i386|server|common|rt|xen)"
-OLDKERNELS=$(dpkg -l|awk '{print $2}'|grep -E $LINUXPKG |grep -vE $METALINUXPKG|grep -v $CURKERNEL)
+KERNELS=$(dpkg -l | awk '/^ii/ && $2 ~ /^linux-image-[0-9]/ { print $2 }' | sort -V)
+CURRENT_KERNEL=$(uname -r)
+OLDKERNELS=$(echo "$KERNELS" | grep -v "$CURRENT_KERNEL" | head -n -"$KERNEL_KEEP")
 RED="\033[0;31m"; ENDCOLOR="\033[0m"
 
 echo Cleaning script by Mark
